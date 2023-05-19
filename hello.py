6e6c2f5b788d3fa27430e0196c1bec05447944d2
Icaro Werly <icaro.henrique001@gmail.com>
2023-05-19 17:32:45 -0300

    Add: script hello.py multi linguagem

--- hello.py
+++ hello.py
@@ -1,1 +1,37 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+""""Hello Word Multi Linguagem
+
+Exibe a mensagem Hello World na linguagem definida no sistema pela variável
+de ambiente LANG.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+
+import os
+
+__version__ = "0.0.1"
+__author__ = "Icaro Werly"
+__license__ = "Unlicense"
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+	msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+	msg = "Ciao, Mondo!"
+elif current_language == "fr_FR":
+	msg = "Bonjour, Monde!"
+	
+print(msg)
